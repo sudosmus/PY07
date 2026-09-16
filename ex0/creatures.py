090d4267f22583040a5f00926d1580d1c23522fb
Sude Gergeroglu <sudegergeroglu@gmail.com>
2026-09-16 12:04:54 +0200

    trial

---
+++ ex0/creatures.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+class Creature(ABC):
+    def __init__(self, name: str, cr_type: str) ->  None:
+        self.name = name
+        self.cr_type = cr_type
+
+    @abstractmethod
+    def attack(self) -> str:
+        pass
+    
+    def describe(self) -> str:
+        return f"{self.name} is a {self.cr_type} Creature"
+
+class Flameling(Creature):
+    def __init__(self) -> None:
+        super().__init__("Flameling", "Fire")
+    def attack(self) -> str:
+        return "Flameling uses Ember!"
+    
+class Pyrodon(Creature):
+    def __init__(self) -> None:
+        super().__init__("Pyrodon", "Fire/Flying")
+
+    def attack(self) -> str:
+        return "Pyrodon uses Flamethrower!"
+    
+
+class Aquabub(Creature):
+    def __init__(self) -> None:
+        super().__init__("Aquabub", "Water")
+
+    def attack(self) -> str:
+        return "Aquabub uses Water Gun!"
+    
+
+class Torragon(Creature):
+    def __init__(self) -> None:
+        super().__init__("Torragon", "Water")
+
+    def attack(self) -> str:
+        return "Torragon uses Hydro Pump!"
+    
+
